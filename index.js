a45b4a2012156ea7f55b43577f366e5019b8ce65
Jonas Holbech <jofh@kea.dk>
2019-09-26 12:42:31 +0200

    put started

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const form = document.querySelector("form");
+form.setAttribute("novalidate", true);
+form.elements.name.addEventListener("focus", e => {
+  form.elements.name.classList.remove("notValid");
+});
+form.elements.name.addEventListener("blur", e => {
+  if (form.elements.name.checkValidity()) {
+    form.elements.name.classList.remove("notValid");
+  } else {
+    form.elements.name.classList.add("notValid");
+  }
+});
+form.addEventListener("submit", evt => {
+  console.log(evt);
+  evt.preventDefault();
+});
+
 function get() {
   fetch(`https://frontendautmn2019-5ad1.restdb.io/rest/superheroes`, {
     method: "get",
@@ -22,7 +39,7 @@ function addHeroToTheDOM(hero) {
   copy.querySelector("h1").textContent = hero.name;
   copy.querySelector("h2>span").textContent = hero.realname;
   copy.querySelector("p").textContent = hero.powers;
-  console.log(hero.powers.split("\n"));
+  //console.log(hero.powers.split("\n"));
   copy.querySelector("button").addEventListener("click", e => {
     const target = e.target.parentElement;
     target.classList.add("remove");
@@ -40,10 +57,10 @@ get();
 
 function post() {
   const data = {
-    name: "The Backender",
-    realname: "!You?",
-    age: 40,
-    powers: "Node.js\nDatabases"
+    name: form.elements.name.value,
+    realname: form.elements.realname.value,
+    age: form.elements.age.value,
+    powers: form.elements.powers.value
   };
   addHeroToTheDOM(data);
   const postData = JSON.stringify(data);
@@ -76,7 +93,3 @@ function deleteIt(id) {
       //document.querySelector(`.hero[data-heroid="${id}"]`).remove();
     });
 }
-
-document.querySelector("button").addEventListener("click", e => {
-  post();
-});
