6d0deb3091905439fe6f4f7720fdc4e10e365fd9
Jonas Holbech <jofh@kea.dk>
2019-09-29 21:53:47 +0200

    last

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const form = document.querySelector("form");
+const form = document.querySelector("form#addForm");
+const formEdit = document.querySelector("form#editForm");
 form.setAttribute("novalidate", true);
+
 form.elements.name.addEventListener("focus", e => {
   form.elements.name.classList.remove("notValid");
 });
@@ -13,6 +15,11 @@ form.elements.name.addEventListener("blur", e => {
 form.addEventListener("submit", evt => {
   console.log(evt);
   evt.preventDefault();
+});
+
+formEdit.addEventListener("submit", evt => {
+  evt.preventDefault();
+  put();
 });
 
 function get() {
@@ -40,7 +47,7 @@ function addHeroToTheDOM(hero) {
   copy.querySelector("h2>span").textContent = hero.realname;
   copy.querySelector("p").textContent = hero.powers;
   //console.log(hero.powers.split("\n"));
-  copy.querySelector("button").addEventListener("click", e => {
+  copy.querySelector("button.btnDelete").addEventListener("click", e => {
     const target = e.target.parentElement;
     target.classList.add("remove");
 
@@ -51,10 +58,33 @@ function addHeroToTheDOM(hero) {
       }
     });
   });
+
+  copy.querySelector("button.btnEdit").addEventListener("click", e => {
+    fetchAndPopulate(hero._id);
+  });
+
   document.querySelector("#app").prepend(copy);
 }
 get();
-
+function fetchAndPopulate(id) {
+  fetch(`https://frontendautmn2019-5ad1.restdb.io/rest/superheroes/${id}`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5d887443fd86cb75861e25ee",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(e => e.json())
+    .then(heroes => {
+      console.log(heroes);
+      formEdit.elements.name.value = heroes.name;
+      formEdit.elements.realname.value = heroes.realname;
+      formEdit.elements.age.value = heroes.age;
+      formEdit.elements.powers.value = heroes.powers;
+      formEdit.elements.id.value = heroes._id;
+    });
+}
 function post() {
   const data = {
     name: form.elements.name.value,
@@ -79,6 +109,39 @@ function post() {
     });
 }
 
+function put() {
+  let data = {
+    name: formEdit.elements.name.value,
+    realname: formEdit.elements.realname.value,
+    age: formEdit.elements.age.value,
+    powers: formEdit.elements.powers.value
+  };
+  let postData = JSON.stringify(data);
+  const superID = formEdit.elements.id.value;
+  fetch(
+    "https://frontendautmn2019-5ad1.restdb.io/rest/superheroes/" + superID,
+    {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": "5d887443fd86cb75861e25ee",
+        "cache-control": "no-cache"
+      },
+      body: postData
+    }
+  )
+    .then(d => d.json())
+    .then(updatedHero => {
+      //find the parent
+      const parentElement = document.querySelector(
+        `.hero[data-heroid="${updatedHero._id}"]`
+      );
+      // update the dom
+      parentElement.querySelector("h1").textContent = updatedHero.name;
+      parentElement.querySelector("h2>span").textContent = updatedHero.realname;
+      parentElement.querySelector("p").textContent = updatedHero.powers;
+    });
+}
 function deleteIt(id) {
   fetch("https://frontendautmn2019-5ad1.restdb.io/rest/superheroes/" + id, {
     method: "delete",
